5b0563620ee2ebfe62d9a86b71eb4b6a1a051363
LazuliKao <LazuliKao233@outlook.com>
2025-09-23 22:49:54 +0800

    add docker-compose

--- build-image.sh
+++ build-image.sh
@@ -10,5 +10,5 @@ then
     echo $F > version
 fi
 
-./alpine-make-vm-image/alpine-make-vm-image --packages "openssh e2fsprogs-extra" --script-chroot --image-format qcow2 $F.qcow2 -- ./setup.sh
+./alpine-make-vm-image/alpine-make-vm-image --packages "openssh e2fsprogs-extra docker-compose" --script-chroot --image-format qcow2 $F.qcow2 -- ./setup.sh
 bzip2 -z $F.qcow2
